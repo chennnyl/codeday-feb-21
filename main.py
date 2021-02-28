c5ec00371afd79393bcd97ac911615f09fae9252
Lynne Cure <pjh.cure@gmail.com>
2021-02-27 21:39:06 -0500

    more background + physics stuff

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import random
+
 import pygame
 from pygame.locals import *
-from pygame.transform import scale2x
+from pygame.sprite import Sprite, Group
 
 from modules.utils import *
 from modules.state import State
-from modules.objects import Player
+from modules.objects import DefaultPlayer, PhysicsWorld, StaticObject, SpriteSheet
 
 pygame.init()
 
@@ -20,9 +22,27 @@ background = background.convert()
 
 clock = pygame.time.Clock()
 
-playerGroup = pygame.sprite.RenderPlain()
+world = PhysicsWorld(debug=False, display_surface=background)
+otherObjects = Group()
 
-testPlayer = Player("player_idle", playerGroup, num_sprites=2)
+ground1 = StaticObject("ground", colorkey=None)
+ground2 = StaticObject("ground", colorkey=None)
+ground1.rect = (0, toscale.get_height() - ground1.image.get_height(), ground1.image.get_width(), ground1.image.get_height())
+ground2.rect = (ground1.image.get_width(), toscale.get_height() - ground1.image.get_height(), ground2.image.get_width(), ground2.image.get_height())
+
+spikes = SpriteSheet("spikes", otherObjects, num_sprites=17, framerate=10, start_dead=True)
+spikes.rect = pygame.Rect(0,toscale.get_height()-ground1.image.get_height()-spikes.image.get_height(), spikes.image.get_width(), spikes.image.get_height())
+
+grasses = [
+    SpriteSheet("grass", otherObjects, num_sprites=9, framerate=10, start_frame=random.randint(0, 8)) for _ in range(90) 
+]
+for i,grass in enumerate(grasses):
+    grass.rect = pygame.Rect(grass.image.get_width()*i, toscale.get_height()-ground1.image.get_height()-grass.image.get_height(), grass.image.get_width(), grass.image.get_height())
+
+testPlayer = DefaultPlayer(otherObjects)
+
+world.add(testPlayer, ground1, ground2)
+
 testPlayer.rect = (background.get_width()/2, background.get_height()/2)
 
 running = True
@@ -38,11 +58,12 @@ while running:
         if event.type == pygame.KEYDOWN:
             keyInput(event, gameState)
 
-    background.fill((255,0,0))
+    background.fill((125, 233, 255))
 
-    playerGroup.update()
-    playerGroup.draw(background)
-    
+    world.update()
+    world.draw(background)
+    otherObjects.update()
+    otherObjects.draw(background)
 
     toscale.blit(background, (0,0))
     pygame.transform.scale(toscale, (screen.get_size()), screen)
--- modules/objects.py
+++ modules/objects.py
@@ -5,13 +5,18 @@ class SpriteSheet(pygame.sprite.Sprite):
     def __init__(self, spritesheet, *groups, **kwargs):
         super().__init__(*groups)
 
+        self.live = not kwargs.get("start_dead", False)
         self.set_image(spritesheet, **kwargs)
     
     def set_image(self, spritesheet, **kwargs):
         num_sprites = kwargs.get("num_sprites", 1)
         self.framerate = kwargs.get("framerate", 30)
 
-        self.spritesheet,_ = load_image(spritesheet, -1)
+        
+
+        colorkey = kwargs.get("colorkey", -1)
+
+        self.spritesheet,_ = load_image(spritesheet, colorkey)
 
         sprite_width = self.spritesheet.get_width() / num_sprites
         sprite_height = self.spritesheet.get_height()
@@ -22,25 +27,22 @@ class SpriteSheet(pygame.sprite.Sprite):
                 self.spritesheet.subsurface((frame * sprite_width, 0, sprite_width, sprite_height))
             )
         
-        self.image = self.frames[0]
         self.counter = 0
-        self.findex = 0
+        self.findex = kwargs.get("start_frame", 0)
+        self.image = self.frames[self.findex]
+
+
+        self.rect = self.image.get_rect()
 
     def update(self):
-        self.counter += 1
-        if self.counter % self.framerate == 0:
-            self.findex = (self.findex+1) % len(self.frames)
-            self.image = self.frames[self.findex]
+        if self.live:
+            self.counter += 1
+            if self.counter % self.framerate == 0:
+                self.findex = (self.findex+1) % len(self.frames)
+                self.image = self.frames[self.findex]
 
-            self.counter = 0        
+                self.counter = 0        
 
-        
-
-    
-        
-
-    
-    
 
 class BaseBlock(pygame.sprite.Sprite):
     def __init__(self, color, dim, *groups):
@@ -50,7 +52,87 @@ class BaseBlock(pygame.sprite.Sprite):
         self.image.fill(color)
 
         self.rect = self.image.get_rect()
-        
-class Player(SpriteSheet):
+
+class PhysicsObject(SpriteSheet):
     def __init__(self, spritesheet, *groups, **kwargs):
+
+        self.set_colliders = PhysicsObject.default_hitbox
+
+        if (custom_func := kwargs.get("set_colliders", None)):
+            self.set_colliders = custom_func
+
         super().__init__(spritesheet, *groups, **kwargs)
+
+
+        self.static = kwargs.get("static", False)
+        
+
+    def set_image(self, spritesheet, *args, **kwargs):
+        super().set_image(spritesheet, *args, **kwargs)
+
+        self.set_colliders(self, *args)
+        
+    def default_hitbox(obj, *args):
+        obj.colliders = {
+            "top": (0, 0, obj.image.get_width(), obj.image.get_height()/2),
+            "bottom": (0, obj.image.get_height()/2, obj.image.get_width(), obj.image.get_height()/2) 
+        }
+        
+class StaticObject(PhysicsObject):
+    def __init__(self, spritesheet, *groups, **kwargs):
+        def set_static_colliders(obj):
+            obj.colliders = {
+                "top": (0,0,obj.image.get_width(), obj.image.get_height()),
+                "bottom": (0,0,0,0)
+            }
+        
+        super().__init__(spritesheet, *groups, set_colliders=set_static_colliders, static=True, **kwargs)
+
+
+class Player(PhysicsObject):
+    def __init__(self, spritesheet=None, *groups, **kwargs):
+        super().__init__(spritesheet, *groups, **kwargs)
+
+
+class DefaultPlayer(Player):
+    def __init__(self, *groups, **kwargs):
+
+        def default_player_hitboxes(obj, *args):
+            if not args:
+                obj.colliders = {
+                    "top": (3, 9, obj.image.get_width()-3, 31),
+                    "bottom": (3, 40, obj.image.get_width()-3, 2+obj.image.get_height()-40)
+                }
+
+
+        super().__init__("player_idle", *groups, num_sprites=2, set_colliders=default_player_hitboxes, **kwargs)
+
+
+class PhysicsWorld(pygame.sprite.Group):
+    def __init__(self, *sprites, **kwargs):
+        super().__init__(*sprites)
+
+        self.debug = kwargs.get("debug", False)
+        self.display_surface = kwargs.get("display_surface", None)
+
+    def add(self, *sprites):
+        if any([True for sprite in sprites if not isinstance(sprite, PhysicsObject)]):
+            raise TypeError("PhysicsWorld may only contain PhysicsObject or descendants")
+    
+        super().add(*sprites)
+    
+    def update(self, *args, **kwargs):
+        super().update(*args, **kwargs)
+
+    def draw(self, *args, **kwargs):
+        super().draw(*args, **kwargs)
+
+        if self.debug:
+            for sprite in self.sprites():
+                for collider in sprite.colliders:
+                    pygame.draw.rect(
+                        self.display_surface, 
+                        (0,255,0), 
+                        pygame.Rect(*sprite.colliders[collider]).move(sprite.rect[0], sprite.rect[1]),
+                        width = 2
+                    )
